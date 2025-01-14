078d3ababfe3632ce7dea478738017fd52e9c754
jai-prakaash <jaisjs123@gmail.com>
2025-01-14 12:31:55 +0530

    Refactor flip coin game HTML and JavaScript files; add new functionality and improve structure

---
+++ FLIP COIN/flipForMatch.js
@@ -0,0 +1,15 @@
+function flipCoin() {
+  const nom = Math.random();
+  console.log(nom);
+  console.log(Math.round(nom));
+
+  let result = "";
+  if (Math.round(nom) === 0) {
+    result = "Heads";
+  } else {
+    result = "Tails";
+  }
+
+  document.querySelector(".result").innerText = result;
+  console.log(result);
+}
